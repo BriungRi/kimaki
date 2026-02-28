a199e75d741b57f70aedf240b765f297f608320b
Brian Li <brnli7@gmail.com>
2026-02-28 03:06:39 +0000

    feat: add guild-scoped event filter

--- discord/src/interaction-handler.ts
+++ discord/src/interaction-handler.ts
@@ -79,7 +79,7 @@ import { handleContextUsageCommand } from './commands/context-usage.js'
 import { handleSessionIdCommand } from './commands/session-id.js'
 import { handleUpgradeAndRestartCommand } from './commands/upgrade.js'
 import { handleModelVariantSelectMenu } from './commands/model.js'
-import { hasKimakiBotPermission } from './discord-utils.js'
+import { hasKimakiBotPermission, isGuildAllowed } from './discord-utils.js'
 import { createLogger, LogPrefix } from './logger.js'
 import { notifyError } from './sentry.js'
 
@@ -98,6 +98,9 @@ export function registerInteractionHandler({
     Events.InteractionCreate,
     async (interaction: Interaction) => {
       try {
+        if (!isGuildAllowed({ guildId: interaction.guildId })) {
+          return
+        }
         interactionLogger.log(
           `[INTERACTION] Received: ${interaction.type} - ${
             interaction.isChatInputCommand()
